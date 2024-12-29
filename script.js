6dba0fb543372103dce53a3e5e38bdd8853df9d9
Mike Wisllen <mikewisllen@gmail.com>
2024-12-28 22:27:49 -0400

    atualização de correção

--- script.js
+++ script.js
@@ -38,3 +38,21 @@
     }
 
 
+    // aplicação da barra lateral mobile
+    let btnMenu = document.getElementById('btn-menu')
+    let menu = document.getElementById('menu-mobile')
+    let overlay = document.getElementById('overlay-menu')
+
+    btnMenu.addEventListener('click', ()=>{
+        menu.classList.add('abrir-menu')
+    })
+
+    menu.addEventListener('click', ()=>{
+        menu.classList.remove('abrir-menu')
+    })
+
+    overlay.addEventListener('click', ()=>{
+        menu.classList.remove('abrir-menu')
+    })
+    // final da aplicação lateral mobile
+
