a3e8765cc5fa440c90f3776db6c2bd460bb8dc7b
Mike Wisllen <mikewisllen@gmail.com>
2025-01-11 16:50:38 -0400

    console.log e deploy

--- api/form.js
+++ api/form.js
@@ -48,4 +48,8 @@ module.exports = async function handler(req, res) {
     res.setHeader('Allow', ['POST', 'OPTIONS']);
     res.status(405).end(`Método ${req.method} não permitido`);
   }
+
+  console.log('EMAIL_USER:', process.env.EMAIL_USER);
+  console.log('EMAIL_PASS:', process.env.EMAIL_PASS ? '******' : 'undefined');
+
 };
