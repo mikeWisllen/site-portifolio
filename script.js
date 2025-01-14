218e26e9d24175a637dff66733eeab205fba818c
Mike Wisllen <mikewisllen@gmail.com>
2025-01-13 20:48:31 -0400

    comunicacao do fronte

--- script.js
+++ script.js
@@ -24,7 +24,7 @@
       const data = Object.fromEntries(formData.entries());
   
       try {
-        const response = await fetch('https://backend-formulario-phi.vercel.app/backend-formulario/server', {
+        const response = await fetch('https://backend-formulario-phi.vercel.app/api/backend-formulario/server', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(data),
